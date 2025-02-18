1ecce7a46927f2059ab3d5b1b0151a1d975d955d
Matheus Felipe <mathfelpessoal@gmail.com>
2025-02-18 20:22:11 -0300

    removing commented code

--- constructions/include/algorithms/GRASP.h
+++ constructions/include/algorithms/GRASP.h
@@ -17,15 +17,6 @@ class GRASP {
 
     ~GRASP();
 
-    // sort the jobs
-    // std::vector<size_t> stpt_sort(const Instance &instance); 
-
-    // calculate the departure time of a job if it were inserted into the solution
-    // std::vector<size_t> computeNewDepartureTime(std::vector<std::vector<size_t>> &d, size_t node);
-
-    // calculate the sum of idle and blocking time
-    // double computeSigma(std::vector<std::vector<size_t>> &d, std::vector<size_t> &newDepartureTime, size_t job, double k);
-
     // construct a solution using the greed GRASP criterion
     Solution solve(double beta);
 
