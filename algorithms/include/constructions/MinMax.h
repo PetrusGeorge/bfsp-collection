cee8b20e37407f84be88cec01487d353fcd2f9a6
sal-mb <samukbore99@gmail.com>
2025-03-22 12:39:21 -0300

    added MinMax

---
+++ algorithms/include/constructions/MinMax.h
@@ -0,0 +1,20 @@
+#ifndef MINMAX_H
+#define MINMAX_H
+#include "Instance.h"
+#include "Parameters.h"
+#include "Solution.h"
+
+class MinMax{
+  public:
+    // alpha parameter value got from Roconi paper, https://doi.org/10.1016/S0925-5273(03)00065-3
+    MinMax(Instance& instance, Parameters& params, double alpha = 0.60);
+    Solution solve();
+
+  private:
+    Instance m_instance;
+    Parameters m_params;
+    double m_alpha;
+
+};
+
+#endif
