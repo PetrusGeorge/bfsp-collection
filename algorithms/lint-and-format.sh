5d610885777324eb8c6884692cc298ab0fa04fca
Guilherme Dantas <gui.dantasp21@gmail.com>
2025-04-02 18:21:05 -0300

    feat: initial solution procedure (PW/PWE2)

--- algorithms/lint-and-format.sh
+++
@@ -1,4 +0,0 @@
-cpp_files=$(find include -type f -name "*.h" && find src -type f -name "*.cpp")
-
-clang-tidy -p build $cpp_files --use-color --fix # Maybe receive build dir as a argument 
-clang-format -i $cpp_files --verbose
