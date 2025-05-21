b7eded13b5c518d6f88c3d7bfe7e468697d1a505
Felipe Carlan <deracine42@proton.me>
2025-05-21 18:19:28 -0300

    Updating stuff

--- SaDIWO/include/RNG.h
+++ SaDIWO/include/RNG.h
@@ -36,7 +36,11 @@ class RNG {
 
     // Random seed if no seed is set
     size_t m_seed = std::random_device{}();
+#ifdef __APPLE__
+    std::mt19937 m_gen{static_cast<unsigned int>(m_seed)};
+#else
     std::mt19937 m_gen{m_seed};
+#endif
 };
 
 #endif
