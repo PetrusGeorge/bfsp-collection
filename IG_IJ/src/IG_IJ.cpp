0017df6beb1914867184e42eff2a45ff22f92ad6
Renan Freire <renanabfreire@q4.logq.int>
2025-05-27 18:28:23 -0300

    benchmark

--- IG_IJ/src/IG_IJ.cpp
+++ IG_IJ/src/IG_IJ.cpp
@@ -84,7 +84,7 @@ Solution IG_IJ::solve() {
 
     VERBOSE(m_params.verbose()) << "Initial solution started\n";
     size_t n = m_instance.num_jobs();
-    size_t lambda = n > 200 ? 20 : n; // setting PF-NEH parameter
+    size_t lambda = n > 200 ? 20 : n; // setting PFT-NEH parameter
 
     PF_NEH pf_neh(m_instance);
     Solution current = pf_neh.solve(lambda);
@@ -96,8 +96,19 @@ Solution IG_IJ::solve() {
     VERBOSE(m_params.verbose()) << current;
 
     // Set time limit to parameter or a default calculation
-    size_t mxn = m_instance.num_jobs() * m_instance.num_machines();
-    size_t time_limit = (m_params.ro() * mxn);
+    size_t time_limit = 0;
+    const size_t mxn = m_instance.num_jobs() * m_instance.num_machines();
+    if (auto tl = m_params.tl()) {
+        time_limit = *tl;
+    } else {
+        time_limit = (m_params.ro() * mxn);
+    }
+
+    std::vector<size_t> ro;
+    if (m_params.becnhmark()) {
+        time_limit = (100 * mxn); // RO == 100
+        ro = {90, 60, 30};
+    }
 
     VERBOSE(m_params.verbose()) << "Time limit: " << time_limit << "s\n";
     NEH neh(m_instance);
@@ -114,12 +125,17 @@ Solution IG_IJ::solve() {
         neh.second_step(std::move(removed), incumbent); // Construct phase
 
         // Local Search
-        if(r < jP){
+        if(r < jP)
             BestSwap(incumbent);
-        }else
+        else{
             rls(incumbent, reference, m_instance);
+        }
 
-        
+        if (!ro.empty() && uptime() >= (ro.back() * mxn)) {
+            std::cout << "rho " << ro.back() << ": " << best.cost << '\n';
+            ro.pop_back();
+        }
+
         //  Program should not accept any solution if the time is out
         if (uptime() > time_limit) {
             break;
