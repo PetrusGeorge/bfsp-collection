761809de6df16c79fbd45442aec1c72353ad5416
Matheus <mathfelpessoal@gmail.com>
2025-05-27 18:21:39 -0300

    .

--- RAIS/include/Instance.h
+++ RAIS/include/Instance.h
@@ -4,32 +4,27 @@
 #include <filesystem>
 #include <vector>
 
-class Instance {
-public:
-  // The constructor of this class can throw in case it can't read the isntance
-  // properly
-  Instance(const std::filesystem::path &path);
+struct Instance {
+  public:
+    Instance(const std::filesystem::path &path);
 
-  size_t num_jobs() const { return m_num_jobs; }
-  size_t num_machines() const { return m_num_machines; }
-  const std::vector<size_t> &processing_times_sum() const {
-    return m_processing_times_sum;
-  }
+    size_t num_jobs() const { return m_num_jobs; }
+    size_t num_machines() const { return m_num_machines; }
+    const std::vector<size_t> &processing_times_sum() const { return m_processing_times_sum; }
 
-  // Main instance matrix
-  long p(size_t i, size_t j) const { return m_matrix[i][j]; }
-  // Reverse matrix
-  long rp(size_t i, size_t j) const {
-    return m_matrix[i][m_num_machines - j - 1];
-  }
+    long p(size_t i, size_t j) const { return m_matrix[i][j]; }
+    // Reverse matrix
+    long rp(size_t i, size_t j) const { return m_matrix[i][m_num_machines - j - 1]; }
 
-private:
-  size_t m_num_jobs = 0;
-  size_t m_num_machines = 0;
-  std::vector<std::vector<long>> m_matrix;
-  std::vector<size_t> m_processing_times_sum;
+    Instance create_reverse_instance();
 
-  void calculate_processing_times_sum();
+  private:
+    size_t m_num_jobs = 0;
+    size_t m_num_machines = 0;
+    std::vector<std::vector<long>> m_matrix;
+    std::vector<size_t> m_processing_times_sum;
+
+    void calculate_processing_times_sum();
 };
 
 #endif
--- RAIS/include/RNG.h
+++ RAIS/include/RNG.h
@@ -3,44 +3,49 @@
 
 #include <random>
 class RNG {
-public:
-  // Singleton pattern
-  RNG(const RNG &) = delete;
-  RNG &operator=(const RNG &) = delete;
-  RNG(RNG &&) = delete;
-  RNG &operator=(RNG &&) = delete;
-  ~RNG() = default;
+  public:
+    // Singleton pattern
+    RNG(const RNG &) = delete;
+    RNG &operator=(const RNG &) = delete;
+    RNG(RNG &&) = delete;
+    RNG &operator=(RNG &&) = delete;
+    ~RNG() = default;
 
-  static RNG &instance() {
-    static RNG instance;
-    return instance;
-  }
+    static RNG &instance() {
+        static RNG instance;
+        return instance;
+    }
 
-  std::mt19937 &gen() { return m_gen; }
-  size_t seed() const { return m_seed; }
-  void set_seed(size_t seed) {
-    m_seed = seed;
-    m_gen.seed(seed);
-  }
+    std::mt19937 &gen() { return m_gen; }
+    size_t seed() const { return m_seed; }
 
-  // If any more functions are needed make them here on this class
-  template <typename T> T generate(T min, T max) {
-    static_assert(std::is_integral_v<T>, "must be an integer type");
-    std::uniform_int_distribution<T> dis(min, max);
-    return dis(m_gen);
-  }
+    void set_seed(size_t seed) {
+        m_seed = seed;
+        m_gen.seed(m_seed);
+    }
 
-  double generate_real_number(double min, double max) {
-    std::uniform_real_distribution<double> dis(min, max);
-    return dis(m_gen);
-  }
+    // If any more functions are needed make them here on this class
+    template <typename T> T generate(T min, T max) {
+        static_assert(std::is_integral_v<T>, "must be an integer type");
+        std::uniform_int_distribution<T> dis(min, max);
+        return dis(m_gen);
+    }
 
-private:
-  RNG() = default;
+    double generate_real_number(double min, double max) {
+        std::uniform_real_distribution<double> dis(min, max);
+        return dis(m_gen);
+    }
 
-  // Random seed if no seed is set
-  size_t m_seed = std::random_device{}();
-  std::mt19937 m_gen{m_seed};
+  private:
+    RNG() = default;
+
+    // Random seed if no seed is set
+    size_t m_seed = std::random_device{}();
+#ifdef __APPLE__
+    std::mt19937 m_gen{static_cast<unsigned int>(m_seed)};
+#else
+    std::mt19937 m_gen{m_seed};
+#endif
 };
 
 #endif
