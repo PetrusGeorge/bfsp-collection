c7ee1c5f3c4a40ce941047cd0ff178eb5112083b
PetrusGeorge <petrus.george.leal@gmail.com>
2025-06-03 20:23:20 -0300

    Add DE_ABC

---
+++ DE_ABC/include/local-search/RLS.h
@@ -0,0 +1,12 @@
+#ifndef RLS_H
+#define RLS_H
+
+#include <vector>
+
+#include "Instance.h"
+#include "Solution.h"
+
+bool rls(Solution &s, Instance &instance);
+bool rls_grabowski(Solution &s, const std::vector<size_t> &ref, Instance &instance);
+
+#endif
